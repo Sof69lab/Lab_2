c54b6cb099fbc5e463eec6dbedbb72917b506b7e
Sof69lab <82488700+Sof69lab@users.noreply.github.com>
2023-12-01 15:40:25 +0700

    Update settings.py

--- testsite/settings.py
+++ testsite/settings.py
@@ -79,8 +79,8 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
         "NAME": "db_2",
-        "USER": "postgres",
-        "PASSWORD": "63618ljv",
+        "USER": "",
+        "PASSWORD": "",
         "HOST": "127.0.0.1",
         "PORT": "5432",
     }
